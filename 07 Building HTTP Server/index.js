05db570f871570736b65a4e8e0a261b09002769c
nilubhai93 <niladrimaji9382@gmail.com>
2025-06-06 00:53:15 +0530

    http server

--- 07 Building HTTP Server/index.js
+++ 07 Building HTTP Server/index.js
@@ -1,16 +1,25 @@
 const http = require("http");
 const fs = require("fs")
 
-const myServer = http.createServer((req,res)=>{
+const myServer = http.createServer((req, res) => {
 
-    const log =`${Date.now()}:New Req Received\n`;
-    fs.appendFile("log.txt",log,(err,data)=>{
-        res.end("Hello from server Again");
+    const log = `${Date.now()}:${req.url} New Req Received\n`;
+    fs.appendFile("log.txt", log, (err, data) => {
+        switch (req.url) {
+            case "/":
+                res.end("HomePage");
+                break;
+            case "/about":
+                res.end("this is about")
+                break;
+            default:
+                res.end("404 error not found")
+        }
     })
-    
+
 });
 
-const port=8000;
-myServer.listen(port,()=>{
+const port = 8000;
+myServer.listen(port, () => {
     console.log(`server startedonpost no ${port}...`)
 })
