e58cc952864d60b472beea4d5c2d828fc6df4bb5
nilubhai93 <niladrimaji9382@gmail.com>
2025-06-10 22:19:01 +0530

    rest api

---
+++ build rest apl/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const app = express();
+
+
+app.get("/users",(req,res)=>{
+    const html =`
+    <ul>
+        ${users.map((user)=>`<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html)
+})
+
+
+
+// REST API
+// app.get("/api/users",(req,res)=>{
+//     return res.send(users);
+// })
+
+// app.get("/api/users/:id",(req,res)=>{
+//     const id = Number(req.params.id);
+//     const user = users.find((user)=>user.id===id);
+//     return res.json(user)
+// })
+
+
+
+app.route("/api/users/:id")
+.get((req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=>user.id===id);
+    return res.json(user)
+})
+.patch((req,res)=>{
+    // edit user
+    res.json({status:"Pending"});
+})
+.delete((req,res)=>{
+    // delete user
+    res.json({hi:"hi"});
+})
+
+
+
+
+
+
+
+
+const PORT = 8000; 
+app.listen(PORT,()=>{
+    console.log(`Server started at port no ${PORT}... `)
+})
