5749521e9e95bc9419e13b8a3846c466836789dd
nilubhai93 <niladrimaji9382@gmail.com>
2025-06-12 22:32:37 +0530

    fs moduling

--- 05 file handling/file.js
+++ 05 file handling/file.js
@@ -3,10 +3,13 @@ const fs = require("fs");
 // fs.writeFile("./testnew.tex", "hey fgfbgfgbf there",(err)=>{});
 
 
+
 // const result = fs.readFile("./testnew.text","utf-8")
 // console.log(result);
 
 
+
+// Async -  Non-blocking
 // fs.readFile("./testnew.tex","utf-8",(err,result)=>{
 //     if(err){
 //         console.log("error", err);
@@ -16,12 +19,18 @@ const fs = require("fs");
 // })
 
 
+
+
+// sync -  blocking
 // fs.appendFileSync("./test.txt",`${Date.now()} hey how are you`);
 
+// sync -  blocking
 // fs.cpSync("./test.txt", "./copyy.txt");
 
 
 
+
+// Async -  Non-blocking
 // fs.unlink("./copyy.txt", (err) => {
 //     if (err) {
 //         console.log("Error deleting file:", err);
