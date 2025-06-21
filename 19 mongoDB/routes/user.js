55ed20b5feda740d259416c39f3baef8f487c156
nilubhai93 <niladrimaji9382@gmail.com>
2025-06-21 14:40:51 +0530

    mongoDB and MVC

--- 19 mongoDB/routes/user.js
+++ 19 mongoDB/routes/user.js
@@ -1,18 +1,23 @@
 const express = require('express');
-const {handleGetAllUsers,handleGetUserById,handleUpdateUserById,handleDeleteUserById,handleCreateUserById} = require("../controllers/user")
+const { 
+    handleGetAllUsers,
+    handleGetUserById,
+    handleUpdateUserById,
+    handleDeleteUserById,
+    handleCreateUserById } = require("../controllers/user")
 const router = express.Router();
 
 
 
 // Routes
-router.get("/",handleGetAllUsers);
+router.get("/", handleGetAllUsers);
 
 router.route("/:id")
     .get(handleGetUserById)
     .patch(handleUpdateUserById)
     .delete(handleDeleteUserById)
 
-router.post("/",handleCreateUserById)
+router.post("/", handleCreateUserById)
 
 
 modeule.export = router;
