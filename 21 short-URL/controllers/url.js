f136a597514de44538ede85925c12ef588a6a686
nilubhai93 <niladrimaji9382@gmail.com>
2025-06-24 15:23:13 +0530

    url shortener

--- 21 short-URL/controllers/url.js
+++ 21 short-URL/controllers/url.js
@@ -1,8 +1,22 @@
-const {nanoid} = require("nanoid");
+const { nanoid } = require("nanoid");
 const URL = require("../models/url");
 
 
-async function handleGenerateNewShortURL(req,res){
+async function handleGenerateNewShortURL(req, res) {
     const body = req.body;
-    if(!body)
+    if (!body.url) return res.status(400).json({ error: "url is required" })
+    const shortID = nanoid(8);
+
+    await URL.create({
+        shortId: shortID,
+        redirectURL: body.url,
+        visitHistory: [],
+    });
+
+    return res.json({id: shortID});
+
+}
+
+module.exports = {
+    handleGenerateNewShortURL
 }
