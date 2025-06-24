56c682f6561bea4e268c1590d476b556cde671a0
nilubhai93 <niladrimaji9382@gmail.com>
2025-06-24 16:06:59 +0530

    url shortener

--- 21 short-URL/index.js
+++ 21 short-URL/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const urlRoute = require ("./routes/router");
 const app = express();
 const PORT = 8000;
+
+
+
+app.use("/url",urlRoute);
+
+
 
 app.listen(PORT,()=>{
     console.log("Server is running...")
