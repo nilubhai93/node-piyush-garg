a1634182fd48187a7516b69660d6c7a34d72d637
nilubhai93 <niladrimaji9382@gmail.com>
2025-06-24 18:20:27 +0530

    url shortener

--- 21 short-URL/controllers/url.js
+++ 21 short-URL/controllers/url.js
@@ -1,10 +1,10 @@
 const { nanoid } = require("nanoid");
-const URL = require("../models/url");
-
+const URL = require("../models/urll");
 
 async function handleGenerateNewShortURL(req, res) {
     const body = req.body;
     if (!body.url) return res.status(400).json({ error: "url is required" })
+    
     const shortID = nanoid(8);
 
     await URL.create({
@@ -13,10 +13,20 @@ async function handleGenerateNewShortURL(req, res) {
         visitHistory: [],
     });
 
-    return res.json({id: shortID});
-
+    return res.json({ id: shortID });
 }
 
+
+async function handleGetAnalytics(req,res) {
+    const shortId = req.params.shortId;
+    const result= await URL.findOne({shortId});
+    return res.json({
+        totalClicks:result.visitHistory.length,
+        analytics:result.visitHistory })
+
+}
 module.exports = {
     handleGenerateNewShortURL,
+    handleGetAnalytics
 }
+
--- 21 short-URL/index.js
+++ 21 short-URL/index.js
@@ -1,13 +1,32 @@
 const express = require("express");
-const urlRoute = require ("./routes/router");
+const {connectToMongoDB}= require("./connect");
+const urlRouter = require("./routes/user");
+
+const URL = require("./models/urll");
+
 const app = express();
-const PORT = 8000;
+const PORT = 8001;
 
 
+connectToMongoDB("mongodb://localhost:27017/short-url")
+.then(()=>console.log("mongodb connected"))
 
-app.use("/url",urlRoute);
+app.use(express.json());
 
+app.use("/url",urlRouter);
 
+app.get("/:shortId",async (req,res)=>{
+    const shortId = req.params.shortId;
+    await URL.findByIdAndUpdate(
+        {shortId},
+        {$push:
+            {visitHistory:{
+                timestamp:Date.now()},
+            },
+        }
+    );
+    res.redirect(entry.redirectURL);  
+})
 
 app.listen(PORT,()=>{
     console.log("Server is running...")
