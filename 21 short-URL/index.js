0bcf7361736c6f53e392573de141a67506c01a59
nilubhai93 <niladrimaji9382@gmail.com>
2025-06-26 19:03:13 +0530

    url shortener andEjs

--- 21 short-URL/index.js
+++ 21 short-URL/index.js
@@ -38,6 +38,6 @@ app.get("/:shortId",async (req,res)=>{
 })
 
 app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}port ...`)
-    
+    console.log(`Server is running on ${PORT} port ...`)
+
 })
